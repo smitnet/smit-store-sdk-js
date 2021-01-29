c4d995e35cfd4b3545685fbf0fb2b53d21edd5eb
Joey van Wijnbergen <jvanwijnbergen@users.noreply.github.com>
2021-01-29 15:09:54 +0100

    Added Find functionality for searching products by slug or identifier

--- src/resources/products.js
+++ src/resources/products.js
@@ -6,6 +6,16 @@ class ProductResource extends CrudResource {
 
     this.resource = 'products';
   }
+
+  Find(slugOrId = undefined) {
+    if (slugOrId !== undefined) {
+      return this.request
+        .post(`${this.resource}/${slugOrId}`)
+        .then((response) => response);
+    }
+
+    return null;
+  }
 }
 
 export default ProductResource;
